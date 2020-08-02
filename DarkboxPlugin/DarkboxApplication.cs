cc4b642ab5aafefd27b5fe931e680e2f5e91e527
Edouard Marquez <moi@edouard-marquez.me>
2020-08-02 17:04:14 +0200

    DarkboxPlugin

---
+++ DarkboxPlugin/DarkboxApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace DarkboxPlugin
+{
+    using System;
+    using Loupedeck;
+
+    public class DarkboxApplication : ClientApplication
+    {
+        protected override String GetProcessName() => "Darkbox";
+
+        protected override String GetBundleName() => ""; // Darkbox application does not exist on MacOS
+    }
+}
---
+++ DarkboxPlugin/DarkboxPlugin.cs
@@ -0,0 +1,16 @@
+﻿namespace DarkboxPlugin
+{
+    using System;
+    using Loupedeck;
+
+    public class DarkboxPlugin : Plugin
+    {
+        public override void RunCommand(String commandName, String parameter)
+        {
+        }
+
+        public override void ApplyAdjustment(String adjustmentName, String parameter, Int32 diff)
+        {
+        }
+    }
+}
